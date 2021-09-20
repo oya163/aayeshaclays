df8a82576e24e410c82424b55099a9a23b34aae0
Oyesh Mann Singh <oyesh.mann.singh@hp.com>
2021-09-20 10:13:50 -0700

    Added localhost

--- aayeshaclays/settings.py
+++ aayeshaclays/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = str(os.getenv("SECRET_KEY"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['prestige.cor.rd.hpicorp.net', 'localhost']
+ALLOWED_HOSTS = ['prestige.cor.rd.hpicorp.net', 'localhost', '127.0.0.1']
 
 STRIPE_SECRET_KEY = str(os.getenv("STRIPE_KEY"))
 
